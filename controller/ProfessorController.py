25c45e920c51bee2045ea95cc560abc2158663be
Lucas Duarte <lucas.duarte@ifood.com.br>
2019-11-11 21:58:18 -0300

    Creating QrCode generator

--- controller/ProfessorController.py
+++ controller/ProfessorController.py
@@ -25,3 +25,31 @@ def cadastrar_professor():
     except Exception as e:
         print(f"Minha exception: {e}")
         return redirect(url_for('login_controller.menu', response="Erro: Usuario ja cadastrado", _external=True))
+
+@professor_controller.route('/professor/disciplinas', methods=['GET', 'POST'])
+def listar_professor_disciplinas():
+    if not session.get("cookie"):
+        return redirect(url_for('login_controller.login', _external=True))
+    if request.method != 'POST':
+        user_info = session['user_info']
+        dicplinas_do_professor = professorService.listar_disciplinas(user_info, session.get("cookie"))
+        return render_template("professor/exibir_professor_disciplina.html", dicplinas_do_professor=dicplinas_do_professor)
+
+@professor_controller.route('/professor/aulas/', methods=['GET', 'POST'])
+def listar_aulas_professor_disciplinas():
+    if not session.get("cookie"):
+        return redirect(url_for('login_controller.login', _external=True))
+
+    aulas = eval(request.form['aulas'])
+    print(aulas)
+    return render_template("professor/aulas_professor_disciplina.html", aulas=aulas)
+
+@professor_controller.route('/aula/criarPresenca/', methods=['GET', 'POST'])
+def gerar_aula_qr_code():
+    if not session.get("cookie"):
+        return redirect(url_for('login_controller.login', _external=True))
+    aula = request.form['aula']
+    #TODO: Implementar adicionar criacao do endpoint para vincular QRCode ao ID da aula
+    qr_code = professorService.gerar_aula_qr_code(aula)
+    return render_template("professor/aula_qr_code.html")
+
--- service/ProfessorService.py
+++ service/ProfessorService.py
@@ -1,5 +1,7 @@
 import requests
 import os
+import pyqrcode 
+from pyqrcode import QRCode 
 import json
 
 class ProfessorService(object):
@@ -21,3 +23,47 @@ class ProfessorService(object):
         except Exception as e:
             print(e)
             raise e
+    
+    def listar_disciplinas(self, user_info, cookie):
+        print(user_info)
+        headers = {'content-type': "application/json"}
+        cookie = cookie
+        try:
+            session = requests.Session()
+            response = requests.get(f"http://{os.environ.get('URL_APPLICATION','localhost:8080')}/api/disciplinasProfessor/{user_info.get('id')}", 
+                headers=headers, cookies=cookie)
+            
+            print(response.text)
+            
+            if (response.status_code == 200):
+                return response.json()
+            else:
+                raise Exception("Usuario ja cadastrado")
+        except Exception as e:
+            print(e)
+            raise e
+    
+    def listar_aulas(self, user_info, cookie):
+        print(user_info)
+        headers = {'content-type': "application/json"}
+        cookie = cookie
+        try:
+            session = requests.Session()
+            response = requests.get(f"http://{os.environ.get('URL_APPLICATION','localhost:8080')}/api/disciplinasProfessor/{user_info.get('id')}", 
+                headers=headers, cookies=cookie)
+            
+            print(response.text)
+            
+            if (response.status_code == 200):
+                return response.json()
+            else:
+                raise Exception("Usuario ja cadastrado")
+        except Exception as e:
+            print(e)
+            raise e
+    
+    def gerar_aula_qr_code(self, data):
+        url = pyqrcode.create(data, error='L', version=27, mode='binary')
+        url.png("templates/static/qr_code_aula.png", scale=6, module_color=[0, 0, 0, 128], background=[0xff, 0xff, 0xcc])
+        return True
+        
