05410b73c1140dca60052acdd06f5146e27be132
Lucas Duarte <lucas.duarte@ifood.com.br>
2019-11-12 21:15:53 -0300

    Ajustado aulas, turmas, disciplias

--- controller/TurmaController.py
+++ controller/TurmaController.py
@@ -32,3 +32,36 @@ def cadastrar_turma():
     except Exception as e:
         print(f"Minha exception: {e}")
         return redirect(url_for('login_controller.menu', response="Erro: Curso já cadastrado", _external=True))
+
+@turma_controller.route('/listar_turmas/', methods=['GET', 'POST'])
+def listar_turmas():
+    turmas = turmaService.listar_turmas(session.get("cookie"))
+    if request.method != 'POST':
+        print("AGORA VAI")
+        return render_template("turma/exibir_turmas.html", turmas=turmas)
+
+@turma_controller.route('/listar_disciplinas_turma/', methods=['GET', 'POST'])
+def listar_disciplinas_turma():
+    turma = eval(request.form['turma'])
+    disciplinas_turma = turma.get("disciplina_turma")
+    print(disciplinas_turma)
+    return render_template("turma/listar_disciplinas_turma.html", disciplinas_turma=disciplinas_turma)
+
+@turma_controller.route('/cadastrar_aulas/', methods=['GET', 'POST'])
+def cadastrar_aulas():
+    disiplina_turma_id = int(request.form['disciplinas_turma_id'])
+    print(disiplina_turma_id)
+    return render_template("turma/cadastrar_aula.html", disiplina_turma_id=disiplina_turma_id)
+
+@turma_controller.route('/cadastrar_aula/', methods=['GET', 'POST'])
+def cadastrar_aula():
+    disiplina_turma_id = int(request.form['disciplinas_turma_id'])
+    print(disiplina_turma_id)
+
+    return render_template("turma/cadastrar_aula.html", disiplina_turma_id=disiplina_turma_id)
+
+@turma_controller.route('/listar_aulas_disciplina/', methods=['GET', 'POST'])
+def listar_aulas_disciplina():
+    aulas = eval(request.form['aulas'])
+    print(aulas)
+    return render_template("turma/listar_aulas.html", aulas=aulas)
---
+++ service/AulaService.py
@@ -0,0 +1,39 @@
+import requests
+import os
+import json
+
+class AulaService(object):
+    
+    def cadastrar_aula(self, cookie, disciplina_turma_id):
+        print(cookie)
+        payload = {'nome': nome, "ano": ano, "curso": { "id": curso } }
+        headers = {'content-type': "application/json"}
+        cookie = cookie
+        try:
+            session = requests.Session()
+            response = requests.post(f"http://{os.environ.get('URL_APPLICATION','localhost:8080')}/api/turma/", 
+                data=json.dumps(payload), headers=headers, cookies=cookie)
+            print(response.status_code)
+            if (response.status_code == 201 or response.status_code == 200):
+                return response.json()
+            else:
+                raise Exception("Curso ja cadastrado")
+        except Exception as e:
+            print(e)
+            raise e
+
+    def listar_turmas(self, cookie):
+        print(cookie)
+        headers = {'content-type': "application/json"}
+        cookie = cookie
+        try:
+            session = requests.Session()
+            response = requests.get(f"http://{os.environ.get('URL_APPLICATION','localhost:8080')}/api/turmas/", headers=headers, cookies=cookie)
+            print(response.status_code)
+            if (response.status_code == 201 or response.status_code == 200):
+                return response.json()
+            else:
+                raise Exception("Curso ja cadastrado")
+        except Exception as e:
+            print(e)
+            raise e
